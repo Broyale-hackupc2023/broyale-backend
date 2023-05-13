508b6514ab7e12adcbec5a35a2acc75cf149f1a9
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-05-13 18:00:36 +0200

    Connect users to rooms

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ from flask import Flask, request, session
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
+from character import Character
+from dungeon_master import DungeonMaster, Input
+
+from user import User
+from room import Room
+
 
 load_dotenv()
 
@@ -16,13 +22,12 @@ app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 users = {}
-
+rooms = {}
 
 @socketio.on('connect')
 def handle_connect():
     # Save the user's session ID
     session_id = request.sid
-    users[session_id] = None
     print('Client connected:', session_id)
 
 
@@ -30,19 +35,81 @@ def handle_connect():
 def handle_disconnect():
     # Remove the user from the dictionary when they disconnect
     session_id = request.sid
-    username = users[session_id]
-    del users[session_id]
-    print('Client disconnected:', session_id, username)
+    msg = f"Client disconnected: {session_id}"
+    if session_id in users:
+        msg += f" ({users[session_id]})"
+        user = users[session_id]
+        if user.room is not None:
+            user.room.leave(user)
+            send_rooms_update()
+        del users[session_id]
+    print(msg)
 
 
-@socketio.on('message')
-def handle_message(data):
-    # Send the message to the recipient's personal room
-    recipient = data['recipient']
-    message = data['message']
-    sender = users[request.sid]
-    emit('message', {'sender': sender, 'message': message}, room=recipient)
-    print('Message sent from', sender, 'to', recipient, ':', message)
+@socketio.on('login')
+def handle_login(data):
+    # Save the user's name and send them a list of rooms
+    name = data['name']
+    new_user = User(request.sid, name)
+    users[new_user.sid] = new_user
+    send_rooms_update(new_user)
+    print('New user:', name)
+
+
+@socketio.on('create_room')
+def handle_create_room(data):
+    # Save the room and send an update to all users
+    name = data['name']
+    description = data['description']
+    owner = users[request.sid]
+    room_id = len(rooms)
+    while room_id in rooms:
+        room_id += 1
+    new_room = Room(room_id, name, description, owner)
+    rooms[room_id] = new_room
+    send_rooms_update()
+    print('New room:', name)
+
+
+@socketio.on('join_room')
+def handle_join_room(data):
+    # Join the room and send an update to all users
+    room_id = data['room_id']
+    room = rooms[room_id]
+    user = users[request.sid]
+    user.join(room)
+    send_rooms_update()
+    print(user, 'joined', room)
+
+
+@socketio.on('leave_room')
+def handle_leave_room():
+    # Leave the room and send an update to all users
+    user = users[request.sid]
+    user.leave_room()
+    send_rooms_update()
+    print(user, 'left the room')
+
+
+
+
+
+def send_rooms_update(user=None):
+    # Sends a list of rooms all users
+    # If a user is specified, only send the list to that user
+    rooms_list = []
+    for room in rooms.values():
+        rooms_list.append(room.to_dict())
+    event = 'rooms_update'
+
+    print("USERS LIST: ", users)
+    if user is None:
+        for user in users.values():
+            emit(event, rooms_list, room=user.sid) 
+    else:
+        emit(event, rooms_list, room=user.sid)
+        
+
 
 
 if __name__ == '__main__':
---
+++ user.py
@@ -0,0 +1,31 @@
+
+
+class User:
+
+	def __init__(self, sid, name):
+		self.sid = sid
+		self.name = name
+		self.room = None
+
+
+	def __str__(self):
+		return self.name
+
+
+	def join(self, room):
+		if self.room is not None:
+			self.room.remove_user(self)
+		room.add_user(self)
+
+
+	def leave_room(self):
+		if self.room is not None:
+			self.room.remove_user(self)
+			self.room = None
+
+
+	def to_dict(self):
+		return {
+			'id': self.sid,
+			'name': self.name,
+		}
