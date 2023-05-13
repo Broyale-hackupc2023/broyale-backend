94bb6c945ac9cafb7a437a3db1a98b11523596da
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2023-05-13 03:18:01 +0200

    Basic server structure

---
+++ server.py
@@ -0,0 +1,71 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, request, session
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_ALLOW_HEADERS'] = 'Content-Type'
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+users = {}
+
+
+@socketio.on('connect')
+def handle_connect():
+    # Save the user's session ID
+    session_id = request.sid
+    users[session_id] = None
+    print('Client connected:', session_id)
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    # Remove the user from the dictionary when they disconnect
+    session_id = request.sid
+    username = users[session_id]
+    del users[session_id]
+    print('Client disconnected:', session_id, username)
+
+
+@socketio.on('join')
+def handle_join(data):
+    # Update the user's username and join their personal room
+    session_id = request.sid
+    username = data['username']
+    users[session_id] = username
+    join_room(session_id)
+    print('User joined:', username)
+
+
+@socketio.on('leave')
+def handle_leave():
+    # Remove the user from their personal room
+    session_id = request.sid
+    leave_room(session_id)
+    print('User left:', users[session_id])
+
+
+@socketio.on('message')
+def handle_message(data):
+    # Send the message to the recipient's personal room
+    recipient = data['recipient']
+    message = data['message']
+    sender = users[request.sid]
+    emit('message', {'sender': sender, 'message': message}, room=recipient)
+    print('Message sent from', sender, 'to', recipient, ':', message)
+
+
+if __name__ == '__main__':
+	host = os.getenv('HOST')
+	port = int(os.getenv('PORT'))
+
+	print(f'Starting server on {host}:{port}')
+	socketio.run(app, host=host, port=port)
